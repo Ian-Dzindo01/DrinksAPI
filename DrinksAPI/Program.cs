484b0c7e88e0e81354838f9cf24718a570eac864
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-02-26 19:51:21 +0100

    Initial commit

---
+++ DrinksAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+// Accept header specifies media types that the client is willing to receive in the response.
+// Clear previous headers
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add( 
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+
+}
